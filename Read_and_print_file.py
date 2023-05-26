4e21c76f835e4adac3227e5d311feae206144000
Yuvrajzzz <134673155+Yuvrajzzz@users.noreply.github.com>
2023-05-26 10:21:10 +0530

    Create Read_and_print_file.py

---
+++ Read_and_print_file.py
@@ -0,0 +1,17 @@
+L = ["Yuvraj\n", "for\n", "Tomar\n"]
+
+file1 = open('myfile.txt', 'w')
+
+file1.writelines(L)
+
+file1.close()
+
+file1 = open('myfile.txt', 'r')
+
+Lines = file1.readlines()
+
+count = 0
+
+for line in Lines:
+
+	count += 1	print("Line{}: {}".format(count, line.strip()))
